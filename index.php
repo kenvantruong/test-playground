41d2877e311ee696c306326849e0a8612af0a47f
Ken Truong <kenvantruong@gmail.com>
2018-12-02 14:58:54 -0800

    Update index.php

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
         $html = "";
         $url = "https://answers.yahoo.com/rss/allq?filter=intl&tab=popular";
         $xml = simplexml_load_file($url);
-        for($i = 0; $i < 1; $i++){
+        for($i = 0; $i < 3; $i++){
             
         # Set Variables from RSS
         $title = $xml->channel->item[$i]->title;
