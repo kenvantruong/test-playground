7f7de5abcd992a5cd7606060a0f2ec10cb9ed527
Ken Truong <kenvantruong@gmail.com>
2018-12-01 16:27:14 -0800

    Add files via upload

---
+++ getrss.php
@@ -0,0 +1,24 @@
+<?php
+        $html = "";
+        $url = "https://answers.yahoo.com/rss/allq?filter=intl&tab=popular";
+        $xml = simplexml_load_file($url);
+        for($i = 0; $i < 2; $i++){
+            
+        # Set Variables from RSS
+        $description = $xml->channel->item[$i]->description;
+
+        # HTML Format
+        $html .= "
+        <div>
+        </ hr>
+            <h5>
+            $description
+            </h5>
+        </div>
+        ";
+        }
+
+
+        
+        echo $html;
+        ?>
